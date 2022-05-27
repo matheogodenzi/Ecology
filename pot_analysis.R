a98f258bad41f0eb6c5fe11c85f8339ded743405
matheogodenzi <matheo.godenzi@epfl.ch>
2022-05-27 20:09:52 +0200

    1st graph

--- pot_analysis.R
+++ pot_analysis.R
@@ -30,3 +30,37 @@ dif3<- T_Humidity_3 - C_Humidity_3
 dif1
 dif2
 dif3
+
+
+## set up some fake test data
+time <- seq(0,72,12)
+betagal.abs <- c(0.05,0.18,0.25,0.31,0.32,0.34,0.35)
+cell.density <- c(0,1000,2000,3000,4000,5000,6000)
+
+## add extra space to right margin of plot within frame
+par(mar=c(5, 4, 4, 6) + 0.1)
+
+## Plot first set of data and draw its axis
+plot(time, betagal.abs, pch=16, axes=FALSE, ylim=c(0,1), xlab="", ylab="", 
+     type="b",col="black", main="Mike's test data")
+axis(2, ylim=c(0,1),col="black",las=1)  ## las=1 makes horizontal labels
+mtext("Beta Gal Absorbance",side=2,line=2.5)
+box()
+
+## Allow a second plot on the same graph
+par(new=TRUE)
+
+## Plot the second plot and put axis scale on right
+plot(time, cell.density, pch=15,  xlab="", ylab="", ylim=c(0,7000), 
+     axes=FALSE, type="b", col="red")
+## a little farther out (line=4) to make room for labels
+mtext("Cell Density",side=4,col="red",line=4) 
+axis(4, ylim=c(0,7000), col="red",col.axis="red",las=1)
+
+## Draw the time axis
+axis(1,pretty(range(time),10))
+mtext("Time (Hours)",side=1,col="black",line=2.5)  
+
+## Add Legend
+legend("topleft",legend=c("Beta Gal","Cell Density"),
+       text.col=c("black","red"),pch=c(16,15),col=c("black","red"))
