9a8c59214b838ce6f2ed59458a38895d43b6e7e5
matheogodenzi <matheo.godenzi@epfl.ch>
2022-06-02 12:58:55 +0200

    new tests

--- pot_analysis.R
+++ pot_analysis.R
@@ -40,7 +40,7 @@ dif3
 data <- FunEco_OUTSIDE
 
 # prend l'horodatage des trois périodes de 24h précédent les mesures de terrain
-dates_1 <- c(data[ (293:341),1])
+dates_1 <- c(data[ (293:340),1])
 dates_2 <- c(data[ (1301:1348),1])
 dates_3 <- c(data[ (1973:2020),1])
 
@@ -66,7 +66,9 @@ precip_1 <- as.numeric(unlist(c(data[ (293:340),5])))
 precip_2 <- as.numeric(unlist(c(data[ (1301:1348),5])))
 precip_3 <- as.numeric(unlist(c(data[ (1973:2020),5])))
 
-?plot
+
+
+### plot first time
 
 ## set up some fake test data
 #time <- c(12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5, 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5)
@@ -102,4 +104,82 @@ mtext("Time (Hours)",side=1,col="black",line=2.5)
 ## Add Legend
 legend("topleft",legend=c("Light Intensity","Temperature Evolution"),
        text.col=c("black","red"),pch=c(16,15),col=c("black","red"))
+
+
+?plot
+
+
+### plot second time
+
+## add extra space to right margin of plot within frame
+par(mar=c(5, 4, 4, 6) + 0.1)
+
+## Plot first set of data and draw its axis
+
+plot(time, sun_2, pch=16, axes=FALSE, ylim=c(0,4000), xlab="", ylab="", 
+     type="l",col="black", main="2nd Campaign")
+axis(2, ylim=c(0,1),col="black",las=1)  ## las=1 makes horizontal labels
+mtext("PAR (µmol/m^2/s)",side=2,line=2.5)
+box()
+
+## Allow a second plot on the same graph
+par(new=TRUE)
+
+## Plot the second plot and put axis scale on right
+plot(time, temp_2, pch=15,  xlab="", ylab="", ylim=c(0,30), 
+     axes=FALSE, type="l", col="red")
+
+## a little farther out (line=4) to make room for labels
+mtext("Temperature",side=4,col="red",line=4) 
+axis(4, ylim=c(0,30), col="red",col.axis="red",las=1)
+
+## Draw the time axis
+axis(1,pretty(range(time),10))
+mtext("Time (Hours)",side=1,col="black",line=2.5)  
+
+## Add Legend
+legend("topleft",legend=c("Light Intensity","Temperature Evolution"),
+       text.col=c("black","red"),pch=c(16,15),col=c("black","red"))
+
+
+### rd plot
+
+?plot
+
+## add extra space to right margin of plot within frame
+par(mar=c(5, 4, 4, 6) + 0.1)
+
+## Plot first set of data and draw its axis
+
+plot(time, sun_3, pch=16, axes=FALSE, ylim=c(0,4000), xlab="", ylab="", 
+     type="l",col="black", main="3st Campaign")
+axis(2, ylim=c(0,1),col="black",las=1)  ## las=1 makes horizontal labels
+mtext("PAR (µmol/m^2/s)",side=2,line=2.5)
+box()
+
+## Allow a second plot on the same graph
+par(new=TRUE)
+
+## Plot the second plot and put axis scale on right
+plot(time, temp_3, pch=15,  xlab="", ylab="", ylim=c(0,30), 
+     axes=FALSE, type="l", col="red")
+
+## a little farther out (line=4) to make room for labels
+mtext("Temperature",side=4,col="red",line=4) 
+axis(4, ylim=c(0,30), col="red",col.axis="red",las=1)
+
+## Draw the time axis
+axis(1,pretty(range(time),10))
+mtext("Time (Hours)",side=1,col="black",line=2.5)  
+
+## Add Legend
+legend("topright",legend=c("Light Intensity","Temperature Evolution"),
+       text.col=c("black","red"),pch=c(16,15),col=c("black","red"))
+
+## Allow a third plot on the same graph
+par(new=TRUE)
+
+plot(time, dif3, pch=15,  xlab="", ylab="", ylim=c(0,1), 
+     axes=FALSE, type="p", col="red")
+
 
